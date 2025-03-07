f9f8448f997004a47c6c10f68e436e4dfabe66e4
AngelAlvarez52 <a22311093@uthermosillo.edu.mx>
2025-03-06 17:06:55 -0700

    Primer commit

---
+++ src/data/user.js
@@ -0,0 +1,67 @@
+const INFO = {
+	main: {
+		title: "Portafolio de Angel Alvarez",
+		name: "Angel Alvarez",
+		email: "angelouss_08@hotmail.com",
+		logo: "../logo.png",
+		
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/AngelAlvarez52",
+		linkedin: "https://www.linkedin.com/in/angelalvarez520/",
+		instagram: "https://www.instagram.com/angelalvarez38/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web y mobile app developer.",
+		description:
+			"Soy un desarrollador Full-Stack especializado en la creación de aplicaciones web y móviles. Con un enfoque en la eficiencia, escalabilidad y experiencia de usuario, transformo ideas en productos digitales funcionales y atractivos. Explora mi trabajo y descubre cómo puedo aportar soluciones innovadoras a tus proyectos.",
+	},
+
+	about: {
+		title: "Soy Angel Álvarez, desarrollador Full-Stack apasionado por la creación y el aprendizaje continuo. ",
+		description:
+			"Desarrollo aplicaciones web y móviles, combinando frontend y backend para crear soluciones funcionales y escalables. Me especializo en construir experiencias intuitivas, optimizadas y eficientes, utilizando tecnologías modernas. Además, disfruto colaborar en proyectos de código abierto y explorar nuevas herramientas que mejoren mi flujo de trabajo. Siempre busco aprender y enfrentar nuevos desafíos en el mundo del desarrollo.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "WishList",
+			description:
+				"Creé una app en Flutter que permite a los usuarios guardar artículos favoritos y compartir su lista de deseos con otros, para que puedan encontrar ideas de regalos.",
+			logo: "https://storage.googleapis.com/cms-storage-bucket/847ae81f5430402216fd.svg",
+			linkText: "View Project",
+			link: "https://github.com/AngelAlvarez52/WishList-Flutter",
+		},
+
+		{
+			title: "CG SUITE",
+			description:
+				"CG SUITE es una app web en la que se gestiona correspondencia y agenda de la empresa Rombotech.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/php/php.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				".",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
